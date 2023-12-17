a39fc06cf6759d82ee31a075d6e9c97cd248a79e
sepeuuu <daluz.270473@balagtas.sti.edu.ph>
2023-12-18 03:34:03 +0800

    Add project files.

---
+++ 07labDALUZ/FrmClubRegistration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07labDALUZ
+{
+    public partial class FrmClubRegistration : Form
+    {
+        private ClubRegistrationQuery clubRegistrationQuery;
+        private int ID, Age, count;
+        private string FirstName, MiddleName, LastName, Gender, Program;
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            FrmUpdateMember frmUpdateMember = new FrmUpdateMember();
+            frmUpdateMember.ShowDialog();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshListOfMembers();
+        }
+
+        private void FrmClubRegistration_Load(object sender, EventArgs e)
+        {
+            RefreshListOfMembers();
+        }
+
+        private long StudentId;
+
+        public FrmClubRegistration()
+        {
+            InitializeComponent();
+            clubRegistrationQuery = new ClubRegistrationQuery();
+            this.Load += FrmClubRegistration_Load;
+        }
+        public void RefreshListOfMembers()
+        {
+            clubRegistrationQuery.DisplayList();
+            dataGridView1.DataSource = clubRegistrationQuery.bindingSource;
+        }
+
+        private int RegistrationID()
+        {
+            count++;
+            return count;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ID = RegistrationID();
+            FirstName = txtFname.Text;
+            MiddleName = txtMname.Text;
+            LastName = txtLname.Text;
+            Gender = cbGender.SelectedItem.ToString();
+            Program = cbProgram.Text;
+
+            Age = int.Parse(txtAge.Text);
+            StudentId = long.Parse(txtStudID.Text);
+
+            clubRegistrationQuery.RegisterStudent(ID, StudentId, FirstName, MiddleName, LastName, Age, Gender, Program);
+
+            RefreshListOfMembers();
+        }
+    }
+}
---
+++ 07labDALUZ/FrmUpdateMember.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace _07labDALUZ
+{
+    partial class FrmUpdateMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbProgram = new System.Windows.Forms.ComboBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.cbStudentid = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Student ID";
+            // 
+            // txtMname
+            // 
+            this.txtMname.Location = new System.Drawing.Point(25, 209);
+            this.txtMname.Multiline = true;
+            this.txtMname.Name = "txtMname";
+            this.txtMname.Size = new System.Drawing.Size(240, 24);
+            this.txtMname.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 15);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Middle Name";
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(25, 155);
+            this.txtFname.Multiline = true;
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(240, 24);
+            this.txtFname.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "First Name";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(24, 101);
+            this.txtLname.Multiline = true;
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(241, 24);
+            this.txtLname.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Last Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 294);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 15);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Gender";
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cbGender.Location = new System.Drawing.Point(24, 317);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(240, 24);
+            this.cbGender.TabIndex = 25;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(27, 263);
+            this.txtAge.Multiline = true;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(239, 24);
+            this.txtAge.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 15);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Age";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 348);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Program";
+            // 
+            // cbProgram
+            // 
+            this.cbProgram.FormattingEnabled = true;
+            this.cbProgram.Items.AddRange(new object[] {
+            "BS in Information Technology (BSIT)",
+            "BS in Computer Science (BSCS)",
+            "BS in Information Systems (BSIS)",
+            "BS in Business Administration (BSBA)",
+            "BS in Accountancy (BSA)",
+            "BS in Accounting Information System (BSAIS)",
+            "BS in Management Accounting (BSMA)",
+            "BS in Retail Technology and Consumer Science (BSRTCS)",
+            "BS in Hospitality Management (BSHM)",
+            "BS in Culinary Management (BSCM)",
+            "BS in Tourism Management (BSTM)",
+            "BS in Computer Engineering (BSCpE)",
+            "Bachelor of Multimedia Arts (BMMA)"});
+            this.cbProgram.Location = new System.Drawing.Point(24, 371);
+            this.cbProgram.Name = "cbProgram";
+            this.cbProgram.Size = new System.Drawing.Size(241, 24);
+            this.cbProgram.TabIndex = 27;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnConfirm.FlatAppearance.BorderSize = 0;
+            this.btnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirm.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.ForeColor = System.Drawing.Color.White;
+            this.btnConfirm.Location = new System.Drawing.Point(85, 415);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(123, 33);
+            this.btnConfirm.TabIndex = 29;
+            this.btnConfirm.Text = "CONFIRM";
+            this.btnConfirm.UseVisualStyleBackColor = false;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // cbStudentid
+            // 
+            this.cbStudentid.FormattingEnabled = true;
+            this.cbStudentid.Location = new System.Drawing.Point(26, 47);
+            this.cbStudentid.Name = "cbStudentid";
+            this.cbStudentid.Size = new System.Drawing.Size(241, 24);
+            this.cbStudentid.TabIndex = 30;
+            this.cbStudentid.SelectedIndexChanged += new System.EventHandler(this.cbStudentid_SelectedIndexChanged);
+            // 
+            // FrmUpdateMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 463);
+            this.Controls.Add(this.cbStudentid);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbProgram);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmUpdateMember";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmUpdateMember";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbProgram;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.ComboBox cbStudentid;
+    }
+}
